b864d758bc436e6eb24a7971792a46b6bb51869b
Tiago-Cassol <tiago-cassol@hotmail.com>
2022-06-19 23:29:59 -0300

    comando de inserção em tese funcionaria, biblioteca de parser morta

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import psycopg2
+from pyparsing import *
 from Loadini import load
 
 def connecta():
@@ -6,10 +7,10 @@ def connecta():
     conn = None
     try:
         ini = load()
-        conn = psycopg2.connect(**ini)
+        conn = psycopg2.connect(*ini)
         cur = conn.cursor()
-        
-        comandoteste='SELECT * from empregados;'
+
+        comandoteste='SELECT from empregados;'
         cur.execute(comandoteste)
         lancacomando = cur.fetchall()
         print(lancacomando)
@@ -18,41 +19,59 @@ def connecta():
 
     except (Exception, psycopg2.DatabaseError) as errou:
         print(errou)
-        
+
 
 def leitura():
     file = open("teste02.txt", 'r')
 
     linhas = file.readlines()
+    global data
     data=[]
-    
+
+    i=0
+    for line in linhas:
 
 
-    for line in reversed(linhas):
-
-        
 
         details = line.split(",")
+
         details = [x.strip() for x in details]
         details = [x.strip('<') for x in details]
         details = [x.strip('>') for x in details]
-        details = [x.strip(']') for x in details]
-        
-        print(details)
-        
+
+        #print(details)
+
         #Desgraca, aaaaaaaaaaaaaaaaaaaa, fazer funcionar e ver direito
+
+
+        dets=Word(alphas)
+        dets.parseString(details[0])
+        #if data[i]=='End CKPT':
+        #    print(data[i+1])
         data.append(details)
-        #print(data)
-        if details==("End CKPT"):
+        print(dets)
+        #parseString(details[i])
+        if data[i]==("End CKPT"):
             break
-    
+        i=i+1
 
 
-        
-    
+
+
+
+
 
 
     #cur = conn.cursor()
+
+
+
+def insercao():
+    cur = conn.cursor()
+    for i in range(len(data)):
+        commands=['begin;', 'insert into empregados(id, name, value) values({data[i]});', 'commit;']
+
+        cur.execute(commands)
 
 def close():
 
@@ -60,7 +79,12 @@ def close():
             conn.close()
             print('Fechou.')
 
-if __name__ == '__main__':
+
+
+
+if name == 'main':
     connecta()
     leitura()
+    #insercao()
+
     close()
